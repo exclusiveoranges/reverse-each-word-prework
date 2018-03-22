df77b92a319399a7b1102ab305b8527b841343bf
Ryan O'Toole <otoolerj@gmail.com>
2018-03-22 17:36:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,16 @@
-def reverse_each_word(string)
-  array = string.split
-  new_array = []
-  array.each do |element|
-  new_array.push(element.reverse)
-  end
-new_array.join(" ")
-end
+# def reverse_each_word(string)
+#   array = string.split
+#   new_array = []
+#   array.each do |element|
+#   new_array.push(element.reverse)
+#   end
+# new_array.join(" ")
+# end
 
 def reverse_each_word(string)
   array = string.split
   array.collect do |element|
-  (element.reverse).join(" ")
+  new_array = element.reverse
+  .join(" ")
   end
 end
