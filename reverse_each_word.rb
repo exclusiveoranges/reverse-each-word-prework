a659db65bab23a37c7389f8d46154d848ab819f1
Ryan O'Toole <otoolerj@gmail.com>
2018-03-22 17:59:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(string)
+#   array = string.split
+#   new_array = []
+#   array.each do |element|
+#   new_array.push(element.reverse)
+#   end
+# new_array.join(" ")
+# end
+
+def reverse_each_word(string)
+  array = string.split
+  reverse_variable = array.collect do |element|
+  element.reverse
+  end
+  reverse_variable.join(" ")
+end
